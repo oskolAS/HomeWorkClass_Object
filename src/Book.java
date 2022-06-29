3ea733bbeb09ca631b0e733819909383dc59c7ef
oskol <oskols@mail.ru>
2022-06-29 22:31:51 +0300

    Готовая домашка!

--- src/Book.java
+++ src/Book.java
@@ -1,1 +1,28 @@
 
+public class Book {
+    private final String nameBooks;
+    private final Author author;
+    int manufacturerYear;
+    //
+    public Book(String nameBooks, Author author, int manufacturerYear) {
+        this.nameBooks = nameBooks;
+        this.author = author;
+        this.manufacturerYear = manufacturerYear;
+    }
+
+    public String getNameBooks() {
+        return nameBooks;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getManufacturerYear() {
+        return manufacturerYear;
+    }
+
+    public void setManufacturerYear(int newManufacturerYear) {
+        this.manufacturerYear = newManufacturerYear;
+    }
+}
